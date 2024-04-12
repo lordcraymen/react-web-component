e50f1f7379ba3b8344b9750e754d972f83d4026a
Florian Patzke <fpatzke@ab-data.de>
2024-04-12 15:46:54 +0200

    chnaging ComponentBase to events

--- src/classes/ComponenBase.ts
+++ src/classes/ComponenBase.ts
@@ -1,40 +1,69 @@
-class ComponentBase extends HTMLElement {
-    private subscribers = new Map();
-    private unsubscribe = () => {};
-    private attributeChangeHandler?: any;
+const eventProperties = {  bubbles: true, composed: true, cancelable: true };
 
-    constructor(attributeChangeHandler?: any) {
+const subscriptionEvent = new Event('ComponentBase:Subscribe', eventProperties);
+const updateEvent = new Event('ComponentBase:Update', eventProperties);
+const unSubscribtionEvent = new Event('ComponentBase:Unsubscribe', eventProperties);
+
+
+class ComponentBase extends HTMLElement {   
+    private subscribers = new WeakMap<ComponentBase,any>();
+    private state = new Map<string, any>();
+
+    constructor(private attributeChangeHandler?: (name: string, newValue: any) => void) {
         super();
-        this.attributeChangeHandler = attributeChangeHandler;
+        this.addEventListener(subscriptionEvent.type, this.handleSubscribe);
+        this.addEventListener('ComponentBase:Update', this.handleUpdate);
+        this.addEventListener(unSubscribtionEvent.type, this.handleUnSubscribe);
+
     }
 
-    protected subscribe(component) {
-        if (component instanceof ComponentBase && component.parentNode === this) {
-            this.subscribers.set(component, component);
-            return () => this.subscribers.delete(component);
+    public getSubscribers = () => this.subscribers;
+
+    private handleSubscribe = (e) => {
+        console.log(e.target, "is subscribing to updates")
+        if (e.target instanceof ComponentBase) {
+            e.stopPropagation();
+            this.subscribers.set(e.target,e.detail);
         }
-        throw new Error("Failed to subscribe: Component must be a ComponentBase instance and a direct child.");
     }
+
+    private handleUpdate = (e) => {
+        console.log(e.target, "is sending an update")
+        if (e.target instanceof ComponentBase) {
+            e.stopPropagation();
+            this.subscribers.set(e.target,e.detail);
+        }
+    }
+
+    private handleUnSubscribe = (e) => { e.stopPropagation(); this.subscribers.delete(e.target); }
 
     protected connectedCallback() {
-        this.unsubscribe = this.parentElement instanceof ComponentBase ? this.parentElement.subscribe(this) : () => {};
+        this.dispatchEvent(subscriptionEvent);
     }
 
-    protected attributeChangedCallback(name, previousValue, newValue) {
+    protected attributeChangedCallback(name: string, previousValue: any, newValue: any) {
         if (previousValue !== newValue) {
-            this.attributeChangeHandler ? this.attributeChangeHandler(name, newValue) : this.onAttributeChange(name, newValue);
-            }
+            this.attributeChangeHandler && this.attributeChangeHandler(name, newValue)
+            this.onAttributeChange(name, newValue)
+        }
     }
-    
-    onAttributeChange(name: any, newValue: any) {}
 
     protected disconnectedCallback() {
-        this.unsubscribe();
-        this.subscribers.clear();
+        this.dispatchEvent(unSubscribtionEvent);
+        this.removeEventListener(subscriptionEvent.type, this.handleSubscribe);
+        this.removeEventListener(unSubscribtionEvent.type, this.handleUnSubscribe);
     }
+    
+    onAttributeChange(name: any, newValue: any) {
+        this.state.set(name, newValue);
+        console.log("attributeChanged!",this.state)
+        this.dispatchEvent(subscriptionEvent);
+        this.dispatchEvent(new CustomEvent('ComponentBase:Update', { detail: this.state , ...eventProperties }))
+    }
+
 }
 
-function createWebComponent(_interface = {}) {
+const createWebComponent = (_interface = {}) =>{
     return class extends ComponentBase {
         static get observedAttributes() {
             return Object.keys(_interface);
