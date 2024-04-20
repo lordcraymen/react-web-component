ba16be5e59ae68594db2436c1639d2a145eec842
Florian Patzke <florian.patzke@t-online.de>
2024-04-20 12:14:26 +0200

    refactored Componetnbase to use a callback on Subscription

--- src/classes/ComponenBase.ts
+++ src/classes/ComponenBase.ts
@@ -1,34 +1,31 @@
-const eventProperties = {  bubbles: true, composed: true, cancelable: true };
+const eventProperties = { bubbles: true, composed: true, cancelable: true };
 
-const UPDATE_EVENT = 'ComponentBase:Update'
-const UNSUBSCRIBE_EVENT = 'ComponentBase:Unsubscribe'
+const SUBSCRIBE_EVENT = 'ComponentBase:Subscribe'
 
+class ComponentBase extends HTMLElement {
 
-class ComponentBase extends HTMLElement {   
-    
     private state;
-    private key;
+    private instanceID;
+    private subscription;
     private componentInterface;
     private handlers;
-    private subscribers = new Map<ComponentBase,any>();
-    private cleanup;
+    private subscribers = new Map<ComponentBase, any>();
 
     static observedAttributes = [];
-    
-    constructor(componentInterface = {},handlers) {
+
+    constructor(componentInterface = {}, handlers) {
         super();
-        this.key = Math.random().toString(36).substring(7);
+        this.instanceID = Math.random().toString(36).substring(7);
+
         this.componentInterface = componentInterface;
+        Object.entries(componentInterface).forEach(([key, value]) => this[key] = value);
+
         this.handlers = handlers;
-        this.cleanup = () => {};
         this.state = {};
-        for (let key in componentInterface) {
-            this[key] = componentInterface[key];
-        }
-        this.addEventListener(UPDATE_EVENT, this.handleChildUpdate);
-        this.addEventListener(UNSUBSCRIBE_EVENT, this.handleUnsubscribe);
+
+        this.addEventListener(SUBSCRIBE_EVENT, this.handleChildSubscription);
     }
-    
+
     protected getSubscribedChildren = () => Array.from(this.subscribers.values())
 
     protected getProps = () => {
@@ -37,70 +34,63 @@ class ComponentBase extends HTMLElement {
                 state[key] = this.attributes.getNamedItem(key)?.value || this.componentInterface[key];
                 return state;
             }, {}),
-            key: this.key,
+            key: this.instanceID,
             children: this.getSubscribedChildren(),
             root: this["root"]
         };
         return props;
     }
-    
 
-    private handleChildUpdate = (e) => {
-       if (e.target instanceof ComponentBase && this !== e.target) {
-            e.stopPropagation();
-            this.subscribers.set(e.target,e.detail);
-            this.state = this.getProps()
-            const update = this.handlers.onUpdate?.(this.state)
-            update && this.dispatchEvent(new CustomEvent(UPDATE_EVENT, {  detail: update, ...eventProperties }))
-        }  
+    private update = () => {
+        const update = this.handlers.onUpdate?.(this.getProps())
+        update && this.subscription?.update(update)
     }
 
-    private handleUnsubscribe = (e) => {
-        if (e.detail.sender instanceof ComponentBase && this.subscribers.has(e.detail.sender)) {
-            console.log("unsubscribing",e.detail.sender,"from",this,"with",this.subscribers.size,"subscribers")
+    private handleChildSubscription = (e) => {
+        if (e.target instanceof ComponentBase && this !== e.target) {
             e.stopPropagation();
-            this.subscribers.delete(e.detail.sender);
-            this.state = this.getProps()
-            const update = this.handlers.onUpdate?.(this.state)
-            update && this.dispatchEvent(new CustomEvent(UPDATE_EVENT, {  detail: update, ...eventProperties }))
+            this.subscribers.set(e.target, null);
+            e.detail.setSubscriptionTo({
+                update: (update) => { this.subscribers.set(this, update); this.update() },
+                unsubscribe: () => { this.subscribers.delete(e.target); this.update() }
+            })
         }
     }
-    
 
     protected connectedCallback() {
-        this.cleanup = this.handlers.onMount?.(this)
-        this.state = this.getProps()
-        console.log(this.state)
+        this.handlers.onMount?.(this)
         const update = this.handlers.onUpdate?.(this.state)
-        update && this.dispatchEvent(new CustomEvent(UPDATE_EVENT, {  detail: update, ...eventProperties }))
+        update && this.parentElement?.dispatchEvent(new CustomEvent(SUBSCRIBE_EVENT, {
+            detail:
+            {
+                setSubscriptionTo: (sub) => this.subscription = sub
+            }, ...eventProperties
+        }))
+        this.update()
     }
- 
-    
+
+
     protected attributeChangedCallback(name: string, previousValue: unknown, newValue: unknown) {
         if (this.state[name] !== newValue) {
             this.state[name] = newValue
-                const update = this.handlers.onUpdate?.(this.state)
-                update && this.dispatchEvent(new CustomEvent(UPDATE_EVENT, {  detail: update, ...eventProperties })) 
+            this.update()
         }
     }
-    
-    
-    protected disconnectedCallback() { // it seems that in this callback the node is already detached from the dom, and parentElement and parentNode are already null.... 
-        //how can I tell the element this element is subscribed to, to remove it from the the parent subscriber map?
-        console.log("unsubscribing",this, this.parentNode)
-        this.removeEventListener(UPDATE_EVENT, this.handleChildUpdate);
+
+
+    protected disconnectedCallback() {
+        this.removeEventListener(SUBSCRIBE_EVENT, this.handleChildSubscription);
         this.subscribers.clear();
-        this.parentElement?.dispatchEvent(new CustomEvent(UNSUBSCRIBE_EVENT, {detail: {sender: this}, ...eventProperties}));
-        //this.cleanup();
+        this.subscription && this.subscription.unsubscribe();
     }
-    
-   
+
+
 }
 
-function createWebComponent(config,handlers) {
+function createWebComponent(config, handlers) {
     return class extends ComponentBase {
         constructor() {
-            super(config,handlers)
+            super(config, handlers)
         }
 
         static get observedAttributes() {
