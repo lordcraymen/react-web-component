7dfd3e8306bca8075a24fd9889c525dc01229b60
florian.patzke@t-online.de <florian.patzke@t-online.de>
2024-05-07 15:41:44 +0200

    chore: Update Layer component with NormalBlending and remove unused code

--- src/components/Layer/Layer.jsx
+++ src/components/Layer/Layer.jsx
@@ -1,32 +1,10 @@
 import { useRef } from 'react';
 import { useThree, createPortal, useFrame } from '@react-three/fiber';
-import { Scene, AmbientLight, NormalBlending } from 'three';
-
+import { Scene, AmbientLight } from 'three';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
-
-const alphaShader = {
-  uniforms: {
-    tDiffuse: { value: null },
-  },
-  vertexShader: `
-    varying vec2 vUv;
-    void main() {
-      vUv = uv;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    }
-  `,
-  fragmentShader: `
-    uniform sampler2D tDiffuse;
-    varying vec2 vUv;
-    void main() {
-      vec4 color = texture2D(tDiffuse, vUv);
-      gl_FragColor = vec4(color.rgb, 0.5);
-    }
-  `,
-};
-
+import { CopyShader } from 'three/examples/jsm/shaders/CopyShader';
 
 const Layer = ({ children }) => {
 
@@ -37,20 +15,43 @@ const Layer = ({ children }) => {
   const composer = useRef(new EffectComposer(gl))
   const basePass = useRef(new RenderPass(scene, camera));
   const pass = useRef(new RenderPass(layerContainer.current, camera));
-  pass.current.clearDepth = true;
 
-  baseComposer.current.addPass(basePass.current);  // Moved this line up
+  //pass.current.clear = false;
+  pass.current.clearDepth = true;
+  baseComposer.current.addPass(basePass.current);
   composer.current.addPass(pass.current);
-  
+
+  const alphaShader = {
+    uniforms: {
+      tDiffuse: { value: null },
+    },
+    vertexShader: `
+      varying vec2 vUv;
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      uniform sampler2D tDiffuse;
+      varying vec2 vUv;
+      void main() {
+        vec4 color = texture2D(tDiffuse, vUv);
+        gl_FragColor = vec4(color.rgb, 0.5);
+      }
+    `,
+  };
+
   const alphaPass = new ShaderPass(alphaShader);
   composer.current.addPass(alphaPass);
-  
+
+  const outputPass = new ShaderPass(CopyShader);
+  outputPass.renderToScreen = true;
+  composer.current.addPass(outputPass);
+
   useFrame(({ gl }) => {
-    
     baseComposer.current.render();
     composer.current.render();
-    
-    
   },1);
 
 
@@ -61,4 +62,4 @@ const Layer = ({ children }) => {
   );
 };
 
-export { Layer };;
+export { Layer };
