ab9816b3d08d56d0783fb35c53e3bc15709e50c6
florian.patzke@t-online.de <florian.patzke@t-online.de>
2024-05-07 14:42:53 +0200

    added effect composer to scene

--- src/components/Layer/Layer.jsx
+++ src/components/Layer/Layer.jsx
@@ -1,27 +1,60 @@
 import { useRef } from 'react';
 import { useThree, createPortal, useFrame } from '@react-three/fiber';
 import { Scene, AmbientLight } from 'three';
-import { CustomBlending, SrcAlphaFactor, OneMinusSrcAlphaFactor, AddEquation } from 'three';
+
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+
+const alphaShader = {
+  uniforms: {
+    tDiffuse: { value: null },
+  },
+  vertexShader: `
+    varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  fragmentShader: `
+    uniform sampler2D tDiffuse;
+    varying vec2 vUv;
+    void main() {
+      vec4 color = texture2D(tDiffuse, vUv);
+      gl_FragColor = vec4(color.rgb, 0.5);
+    }
+  `,
+};
+
 
 const Layer = ({ children }) => {
 
   const { scene, size, gl, camera } = useThree();
-
   const layerContainer = useRef(new Scene().add(new AmbientLight(0xffffff, 1)));
 
-  useFrame(({gl}) => {
-    console.log(gl)
-    // Enable blending
-    //gl.state.enable(gl.BLEND);
-    // Set blending to custom
-    //gl.state.setBlending(CustomBlending);
-    // Set blend equation
-    //gl.state.buffers.color.setBlendEquation(AddEquation);
-    // Set source blend factor (half of the source's alpha)
-    //gl.state.buffers.color.setBlendSrc(SrcAlphaFactor);
-    // Modify destination blend factor to factor in the constant opacity
-    //gl.state.buffers.color.setBlendDst(OneMinusSrcAlphaFactor);
-    gl.render(layerContainer.current, camera);
+  const baseComposer = useRef(new EffectComposer(gl))
+  const composer = useRef(new EffectComposer(gl))
+  const basePass = useRef(new RenderPass(scene, camera));
+  const pass = useRef(new RenderPass(layerContainer.current, camera));
+
+  pass.current.clear = false;
+  pass.current.clearDepth = true;
+  
+  composer.current.addPass(pass.current);
+  const alphaPass = new ShaderPass(alphaShader);
+  composer.current.addPass(alphaPass);
+
+
+  baseComposer.current.addPass(basePass.current);
+
+
+  
+  
+  useFrame(({ gl }) => {
+    //gl.autoClear = false;
+    baseComposer.current.render();
+    composer.current.render();
   },1);
 
 
--- src/components/Scene/Scene.jsx
+++ src/components/Scene/Scene.jsx
@@ -31,7 +31,7 @@ const Scene = ({children}) => {
   const [focusedBox, setFocusedBox] = useState(null); // Track which box is focused
 
   return (
-    <Canvas>
+    <Canvas gl={{ preserveDrawingBuffer: true }}>
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
