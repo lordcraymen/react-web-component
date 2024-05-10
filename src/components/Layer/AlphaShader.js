70bae3831478b7d3e08ca76e5fc90c4537e54abe
Florian Patzke <fpatzke@ab-data.de>
2024-05-10 14:27:27 +0200

    refactor: Update Layer component with AlphaShader for transparent rendering

--- src/components/Layer/AlphaShader.js
+++ src/components/Layer/AlphaShader.js
@@ -1,10 +1,12 @@
 const AlphaShader = {
 
-  name: 'CopyShader',
+  name: 'Alphashader',
 
   uniforms: {
-    'tPass': { value: null },
+    'tPassColor': { value: null },
+    'tPassDepth': { value: null },
     'tDiffuse': { value: null },
+    'tDepth': { value: null },
     'opacity': { value: 1 }
   },
 
@@ -23,20 +25,27 @@ const AlphaShader = {
 
   uniform float opacity;
 
-  uniform sampler2D tPass;
-
+  uniform sampler2D tPassColor;
+  uniform sampler2D tPassDepth;
   uniform sampler2D tDiffuse;
+  uniform sampler2D tDepth;
 
   varying vec2 vUv;
 
   void main() {
+      vec4 originalTexel = texture2D(tDiffuse, vUv);
+      vec4 originalDepth = texture2D(tDepth, vUv);
 
-    vec4 originalTexel = texture2D( tDiffuse, vUv );
-    vec4 passTexel = texture2D( tPass, vUv );
+      vec4 passTexel = texture2D(tPassColor, vUv);
+      vec4 passDepth = texture2D(tPassDepth, vUv);
 
-    vec4 blendedTexel = mix(originalTexel, passTexel, passTexel.a * opacity);
+      // Compare depth values to decide on blending
+      float depthCheck = step(passDepth.r, originalDepth.r);
 
-    gl_FragColor = blendedTexel;
+      // Calculate blended color based on depth check and opacity
+      vec4 blendedTexel = mix(originalTexel, passTexel, passTexel.a * opacity * depthCheck);
+
+      gl_FragColor = blendedTexel;
   }`
 
 };
--- src/components/Layer/Layer.jsx
+++ src/components/Layer/Layer.jsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from 'react';
-import { Scene, AmbientLight, Color } from 'three';
+import { Scene, AmbientLight, DepthTexture } from 'three';
 import { useThree, useFrame, createPortal } from '@react-three/fiber';
 import { EffectComposer,  } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
@@ -10,6 +10,9 @@ const Layer = ({ children, opacity = 1 }) => {
   const { scene, size, gl, camera } = useThree();
   const layerContainer = useRef(new Scene().add(new AmbientLight(0xffffff, 1)));
 
+  const firstDepthTexture = useRef(new DepthTexture(size.width, size.height));
+  const secondDepthTexture = useRef(new DepthTexture(size.width, size.height));
+
   const firstComposer = useRef(new EffectComposer(gl));
   firstComposer.current.setSize(size.width, size.height);
 
@@ -18,26 +21,47 @@ const Layer = ({ children, opacity = 1 }) => {
 
   const firstPass = useRef(new RenderPass(scene, camera));
   const secondPass = useRef(new RenderPass(layerContainer.current, camera));
-  //secondPass.current.clearDepth = true;
-  secondComposer.current.addPass(secondPass.current);
+  secondComposer.current.readBuffer.depthTexture = secondDepthTexture.current;
+  //secondComposer.current.addPass(secondPass.current);
   secondComposer.current.renderToScreen = false;
 
-  firstComposer.current.addPass(firstPass.current);
+  firstComposer.current.readBuffer.depthTexture = firstDepthTexture.current;
+  //firstComposer.current.addPass(firstPass.current);
+  firstPass.current.clearDepth = false;
 
   const finalPass = useRef(new ShaderPass(AlphaShader))
   finalPass.current.renderToScreen = true;
   
-  firstComposer.current.addPass(finalPass.current);
+  //firstComposer.current.addPass(finalPass.current);
 
   const _opacity = parseFloat(String(opacity))
+  gl.autoClear = true;
 
-  useFrame(({ gl }) => {
-    gl.autoClear = false;
+  useFrame(({ gl, scene, camera }) => { 
     secondComposer.current.render();
-    finalPass.current.material.uniforms.tPass.value = secondComposer.current.readBuffer.texture;
+    finalPass.current.material.uniforms.tPassColor.value = secondComposer.current.readBuffer.texture;
+    finalPass.current.material.uniforms.tPassDepth.value = secondComposer.current.readBuffer.depthTexture;
+    finalPass.current.material.uniforms.tDepth.value = firstComposer.current.readBuffer.depthTexture;
     finalPass.current.material.uniforms.opacity.value = _opacity;
     firstComposer.current.render();
-  }, 1);
+},1);
+
+
+  useEffect(() => {
+    secondComposer.current.addPass(secondPass.current);
+    firstComposer.current.addPass(firstPass.current);
+    firstComposer.current.addPass(finalPass.current);
+    return () => {
+      secondComposer.current.removePass(secondPass.current);
+      firstComposer.current.removePass(firstPass.current);
+      firstComposer.current.removePass(finalPass.current);
+      firstComposer.current.dispose();
+      secondComposer.current.dispose();
+      firstDepthTexture.current.dispose();
+      secondDepthTexture.current.dispose();
+    };
+}, []); // Empty dependency array ensures this runs once
+
 
   return (
     <>
