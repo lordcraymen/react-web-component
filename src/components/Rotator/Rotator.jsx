a462d3ffdf32084896026a4710cd240696f06267
Florian Patzke <fpatzke@ab-data.de>
2024-05-17 14:51:40 +0200

    feat: Add mouse-based rotation to Rotator component

--- src/components/Rotator/Rotator.jsx
+++ src/components/Rotator/Rotator.jsx
@@ -4,15 +4,12 @@ import { useThree, useFrame } from '@react-three/fiber';
 
 const Rotator =({children}) => {
     const meshRef = useRef(new Group());
+    const { pointer } = useThree();
 
-    useFrame(({ clock }) => {
-        const time = clock.getElapsedTime();
-        const t = (Math.sin(time) + 1) / 2;  // Normalized time value between 0 and 1
-        const x = t * 2 - 1;  // Maps t to a range between -1 and 1
-        const y = Math.pow(t, 3) * 2 - 1;  // Cubic interpolation for y
-        const z = Math.sin(t * Math.PI) * 2 - 1;  // Sine interpolation for z
-
-        meshRef.current.position.set(x, y, z);
+    useFrame(() => {
+        const rotationX = pointer.y * Math.PI; // Rotate based on Y position of mouse
+        const rotationY = pointer.x * Math.PI; // Rotate based on X position of mouse
+        meshRef.current.rotation.set(rotationX, rotationY, 0);
     });
 
     return (
