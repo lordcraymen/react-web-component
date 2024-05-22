b3d8a5b89343148b3a55308dc1ff40b5ed0ead7b
Florian Patzke <florian.patzke@t-online.de>
2024-05-22 22:24:01 +0200

    refactor: Improve zooming sensitivity in Zoom component

--- src/components/Zoom/Zoom.js
+++ src/components/Zoom/Zoom.js
@@ -1,39 +1,69 @@
-import { useEffect, useRef } from 'react';
+import { useRef, useEffect, useCallback } from 'react';
 import { Vector3 } from 'three';
 import { invalidate, useThree } from '@react-three/fiber';
+
+
+const useLoop = (callback) => {
+  const whileTrue = useRef(true);
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  const loop = useCallback(() => {
+    if (whileTrue.current && typeof callbackRef.current === 'function') {
+      if (callbackRef.current() !== false) {
+        requestAnimationFrame(loop);
+      } else {
+        whileTrue.current = false;
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof callbackRef.current === 'function') {
+      requestAnimationFrame(loop);
+    }
+    return () => {
+      whileTrue.current = false; // Stop the loop on cleanup
+    };
+  }, [loop]);
+
+  return useCallback(() => {
+    if (!whileTrue.current) {
+      whileTrue.current = true;
+      loop();
+    }
+  }, [loop]);
+};
+
+export default useLoop;
+
 
 const Zoom = ({speed = 1}) => {
   const { camera, gl } = useThree();
   const velocity = useRef(0);
-  let animationFrame = -1
-
-  const handleWheel = (event) => {
-    velocity.current += event.deltaY * ({ 1: 16, 2: 100 }[event.deltaMode] || 1) * (speed / 200);
-    if(animationFrame === -1) animate();
-  };
-
-  const animate = () => {
-    console.log('animate');
-    if (velocity.current === 0) { animationFrame = -1; return cancelAnimationFrame(animationFrame); }
-
-    velocity.current *= 0.8;
-
-    if (Math.abs(velocity.current) < 0.001) {
-      velocity.current = 0;
-    } 
-
+  
+  const update = useLoop(() => {
+    if (velocity.current === 0) return false;
+    velocity.current *= 0.9;
     const direction = camera.getWorldDirection(new Vector3()).multiplyScalar(velocity.current);
     camera.parent.worldToLocal(direction);
     camera.position.add(direction);
-    
     invalidate();
-    animationFrame = requestAnimationFrame(animate);
-  }
+    if (Math.abs(velocity.current) < 0.001) velocity.current = 0;
+    return true;
+  })
+
+  const handleWheel = (event) => {
+    velocity.current += event.deltaY * ({ 1: 16, 2: 100 }[event.deltaMode] || 1) * (speed / 200);
+    update();
+  };
 
   useEffect(() => {
     gl.domElement.addEventListener('wheel', handleWheel);
     return () => {
-      cancelAnimationFrame(animationFrame);
       gl.domElement.removeEventListener('wheel', handleWheel)
     };
   }, [gl.domElement]);
