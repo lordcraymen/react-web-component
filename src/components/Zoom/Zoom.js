f8136069c1e36b0b1f8f73d27fe2047aa8e8c87b
Florian Patzke <fpatzke@ab-data.de>
2024-05-22 16:01:18 +0200

    refactor: Improve zooming sensitivity in Zoom component

--- src/components/Zoom/Zoom.js
+++ src/components/Zoom/Zoom.js
@@ -1,35 +1,41 @@
 import { useEffect, useRef } from 'react';
 import { Vector3 } from 'three';
-import { useThree, useFrame } from '@react-three/fiber';
+import { invalidate, useThree } from '@react-three/fiber';
 
 const Zoom = ({speed = 1}) => {
   const { camera, gl } = useThree();
   const velocity = useRef(0);
+  let animationFrame = -1
 
   const handleWheel = (event) => {
-    const deltaMultipliers = { 1: 16, 2: 100 };
-    velocity.current += event.deltaY * (deltaMultipliers[event.mode] || 1) * (speed / 200); // adjust sensitivity
+    velocity.current += event.deltaY * ({ 1: 16, 2: 100 }[event.deltaMode] || 1) * (speed / 200);
+    if(animationFrame === -1) animate();
   };
 
-  useFrame(() => {
-    if (velocity.current !== 0) {
-      // move camera along its viewing direction
-      const direction = camera.getWorldDirection(new Vector3()).multiplyScalar(velocity.current);
-      camera.parent.worldToLocal(direction);
-      camera.position.add(direction);
+  const animate = () => {
+    console.log('animate');
+    if (velocity.current === 0) { animationFrame = -1; return cancelAnimationFrame(animationFrame); }
 
-      // damping
-      velocity.current *= 0.8;
+    velocity.current *= 0.8;
 
-      if (Math.abs(velocity.current) < 0.001) {
-        velocity.current = 0;
-      }
-    }
-  });
+    if (Math.abs(velocity.current) < 0.001) {
+      velocity.current = 0;
+    } 
+
+    const direction = camera.getWorldDirection(new Vector3()).multiplyScalar(velocity.current);
+    camera.parent.worldToLocal(direction);
+    camera.position.add(direction);
+    
+    invalidate();
+    animationFrame = requestAnimationFrame(animate);
+  }
 
   useEffect(() => {
     gl.domElement.addEventListener('wheel', handleWheel);
-    return () => {gl.domElement.removeEventListener('wheel', handleWheel)};
+    return () => {
+      cancelAnimationFrame(animationFrame);
+      gl.domElement.removeEventListener('wheel', handleWheel)
+    };
   }, [gl.domElement]);
 
   return null;
