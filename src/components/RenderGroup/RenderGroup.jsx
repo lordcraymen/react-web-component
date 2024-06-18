bf23c2be6e85a4feb071b5b46d759b3e1b70ccff
florian.patzke@t-online.de <florian.patzke@t-online.de>
2024-06-18 10:45:20 +0200

    refactor: Improve depth testing and transparency in RenderGroup component

--- src/components/RenderGroup/RenderGroup.jsx
+++ src/components/RenderGroup/RenderGroup.jsx
@@ -1,167 +1,18 @@
-/*
-import { useThree, useFrame, Canvas, createPortal} from "@react-three/fiber"
-import { useEffect, useRef, useMemo, useState } from "react"
-import { UnsignedShortType, WebGLRenderer, WebGLRenderTarget, RGBAFormat, ShaderMaterial, DepthTexture, Texture, Vector2 } from "three"
-import { CopyShader } from "three/examples/jsm/shaders/CopyShader"
+import { useEffect, useRef, useMemo } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Vector2, WebGLRenderTarget, ShaderMaterial, DepthTexture } from 'three';
+
+//SkipRenderMaterial is a material that will cause the renderbufferDirect function to return early
+//I use this material to skip rendering the objects that are not in the current render group
+import { SkipRenderMaterial } from '../../shaders/SkipRenderMaterial';
 
 
-const RenderTargetComponent = ({ renderTarget }) => {
-  const [container, setContainer] = useState(null);
-
-  useEffect(() => {
-    // Öffnen Sie ein neues Fenster und speichern Sie das Dokument des Fensters
-    const popupWindow = window.open("", "_blank");
-    setContainer(popupWindow.document.body);
-
-    // Schließen Sie das Fenster, wenn die Komponente unmountet wird
-    return () => popupWindow.close();
-  }, []);
-
-  // Rendern Sie das Portal nur, wenn der Container definiert ist
-  return container
-    ? createPortal(
-        <Canvas>
-          <mesh>
-            <planeBufferGeometry args={[2, 2]} />
-            <meshBasicMaterial map={renderTarget.texture} />
-          </mesh>
-        </Canvas>,
-        container
-      )
-    : null;
-};
-
-
-
-const RenderGroup = ({ children, opacity }) => {
-
-    const secondRenderer = useRef(new WebGLRenderer());
-
-    const groupRef = useRef()
-
-    const DepthMaterial = useRef(new ShaderMaterial({
-        uniforms: {
-            diffuseTexture: { value: null },
-            depthTexture: { value: null },
-            opacity: { value: 1.0 }
-        },
-        vertexShader: CopyShader.vertexShader,
-        fragmentShader: `
-            uniform sampler2D diffuseTexture;
-            uniform sampler2D depthTexture;
-            uniform float opacity;
-            varying vec2 vUv;
-        
-            void main() {
-                float currentDepth = gl_FragCoord.z / gl_FragCoord.w;
-                float depthFromTexture = texture2D(depthTexture, vUv).r;
-                float visibility = step(currentDepth, depthFromTexture);
-                //if (visibility < 0.5) discard;
-                //gl_FragColor = texture2D(diffuseTexture, vUv);
-
-                //gl_FragColor = texture2D(depthTexture, vUv);
-                gl_FragColor = vec4(depthFromTexture, depthFromTexture, depthFromTexture, opacity); // Use the opacity passed to the shader
-            }
-        `,
-        transparent: true,
-        depthWrite: true,
-        name: 'DepthMaterial'
-    }))
-
-    const size = useThree(({ size }) => size)
-
-    const renderTarget = useMemo(() => { 
-        const dt = new DepthTexture(size.width, size.height)
-        dt.type = UnsignedShortType
-        const rt = new WebGLRenderTarget(size.width, size.height, {
-            format: RGBAFormat,
-            depthTexture: dt,
-            depthBuffer: true
-        }) 
-
-        secondRenderer.current.setRenderTarget(rt)
-        return rt
-    }, [size])
-
-    useFrame(({ gl, scene, camera }) => {
-    DepthMaterial.current.opacity = opacity;
-    DepthMaterial.current.transparent = opacity !== 1;
-
-
-    const prevOverrideMaterial = groupRef.current.overrideMaterial;
-    groupRef.current.overrideMaterial = null;
-    secondRenderer.current.render(scene, camera);
-    groupRef.current.overrideMaterial = prevOverrideMaterial;
-
-    DepthMaterial.current.uniforms.diffuseTexture.value = renderTarget.texture;
-    DepthMaterial.current.uniforms.depthTexture.value = renderTarget.depthTexture;
-});
-
-    return <>
-    <group ref={groupRef} overrideMaterial={DepthMaterial.current}>{children}</group>
-    <mesh>
-        <planeGeometry args={[2, 2]} />
-        <meshBasicMaterial map={renderTarget.texture} />
-    </mesh>
-    </>
-
-}
-
-*/
-
-import {
-    useEffect,
-    useRef,
-    useMemo
-} from 'react';
-import {
-    useThree,
-    useFrame
-} from '@react-three/fiber';
-import {
-    useFBO
-} from '@react-three/drei';
-import {
-    Color,
-    Vector2,
-    WebGLRenderTarget,
-    OrthographicCamera,
-    Scene,
-    Mesh,
-    PlaneGeometry,
-    MeshBasicMaterial,
-    ShaderMaterial,
-    DepthTexture,
-    UnsignedShortType,
-    FloatType,
-    RGBAFormat,
-    NearestFilter,
-    GreaterEqualDepth,
-    NotEqualDepth
-} from 'three';
-import {
-    CopyShader
-} from "three/examples/jsm/shaders/CopyShader"
-import {
-    NullShader
-} from './NullShader';
-import {
-    SkipRenderMaterial
-} from '../../shaders/SkipRenderMaterial';
-
-
-let renderOffset = 100;
-
-const RenderGroup = ({
-    children,
-    opacity,
-    zindex = 0
-}) => {
+const RenderGroup = ({children, opacity=1}) => {
     const groupRef = useRef();
 
     const renderTarget = useMemo(() => {
-        const depthBuffer = new DepthTexture(1,1)
-        return new WebGLRenderTarget(1,1, { depthBuffer: true, depthTexture: depthBuffer, name: 'DepthRenderTarget' })
+        const depthBuffer = new DepthTexture(1, 1)
+        return new WebGLRenderTarget(1, 1, { depthBuffer: true, depthTexture: depthBuffer })
     }, [])
 
     useEffect(() => () => { renderTarget.dispose() }, [renderTarget])
@@ -182,62 +33,51 @@ const RenderGroup = ({
             }
         },
         vertexShader: `
-        varying vec2 vUv;
-        void main() {
-            vUv = uv;
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-        }
-    `,
-        fragmentShader: `
-        uniform sampler2D diffuseTexture;
-        uniform sampler2D depthTexture;
-        uniform float opacity;
-        uniform vec2 resolution;
-        varying vec2 vUv;
-
-        void main() {
-            
-
-            float depth = texture2D(depthTexture, gl_FragCoord.xy / resolution).r / gl_FragCoord.w;
-            float currentDepth = (gl_FragCoord.z / gl_FragCoord.w) - 0.001;
-            if (currentDepth > depth) {
-            discard;
+            void main() {
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
             }
-            vec4 texel = texture2D(diffuseTexture, gl_FragCoord.xy / resolution);
-            gl_FragColor = vec4(texel.rgb,texel.a * opacity);
+        `,
+        fragmentShader: `
+            uniform sampler2D diffuseTexture;
+            uniform sampler2D depthTexture;
+            uniform float opacity;
+            uniform vec2 resolution;
 
-        }
-    `,
+            void main() {
+            
+                float depth = texture2D(depthTexture, gl_FragCoord.xy / resolution).r / gl_FragCoord.w;
+                float currentDepth = (gl_FragCoord.z / gl_FragCoord.w) - 0.001;
+            
+                if (currentDepth > depth) {
+                    discard;
+                }
+            
+                vec4 texel = texture2D(diffuseTexture, gl_FragCoord.xy / resolution);
+                gl_FragColor = vec4(texel.rgb,texel.a * opacity);
+            }
+        `,
         depthWrite: false,
-        depthTest: false,
+        depthTest: true,
         name: 'DepthMaterial'
     }))
 
-    DepthMaterial.current.opacity = opacity;
-    DepthMaterial.current.transparent = opacity !== 1;
-    DepthMaterial.current.uniforms.diffuseTexture.value = renderTarget.texture;
-    DepthMaterial.current.uniforms.depthTexture.value = renderTarget.depthTexture;
-    DepthMaterial.current.uniforms.opacity.value = opacity;
+    
 
-    useFrame(({
-        gl,
-        scene,
-        camera,
-        size
-    }) => {
-        
+    useFrame(({ gl, scene, camera, size }) => {
+
         const ratio = gl.getPixelRatio();
         renderTarget.setSize(size.width * ratio, size.height * ratio);
         DepthMaterial.current.uniforms.resolution.value.set(size.width * ratio, size.height * ratio);
 
         gl.setRenderTarget(renderTarget);
 
-        const prevSceneOverrideMaterial = scene.overrideMaterial;
-
+        // I set everything in the scene to a SkipRenderMaterial material
+        // so that the renderbufferDirect function will return early
         scene.traverse(obj => {
-            if (!obj.isScene) obj.overrideMaterial = SkipRenderMaterial;
+            if(!obj.isScene) obj.overrideMaterial = SkipRenderMaterial;
         });
 
+        // I set the current group to null so that objects in this group will render normally
         groupRef.current.traverse(obj => {
             obj.overrideMaterial = null;
         });
@@ -245,17 +85,28 @@ const RenderGroup = ({
         gl.render(scene, camera);
         gl.setRenderTarget(null);
 
-        scene.overrideMaterial = prevSceneOverrideMaterial;
+        //setting the opacity and the texture of the DepthMaterial
+        DepthMaterial.current.opacity = opacity;
+        DepthMaterial.current.transparent = opacity !== 1;
+        DepthMaterial.current.uniforms.diffuseTexture.value = renderTarget.texture;
+        DepthMaterial.current.uniforms.depthTexture.value = renderTarget.depthTexture;
+        DepthMaterial.current.uniforms.opacity.value = opacity;
 
+        // I set ovverideMaterial to null in the scene so that objects in the scene will render normally
+        //todo: maybe store the original overrideMaterials in a map and roll back to them
         scene.traverse(obj => {
-            if (!obj.isScene) obj.overrideMaterial = null
+             obj.overrideMaterial = null
         });
 
-        groupRef.current.traverse(obj => { 
+        // I set the current group to DepthMaterial so that objects in this group 
+        //will render with the DepthMaterial
+        groupRef.current.traverse(obj => {
             obj.overrideMaterial = DepthMaterial.current
-            obj.renderOrder = zindex 
+            // I set the renderOrder to 100 so that the objects in this group will render after the other objects
+            //as the shader needs the depth buffer to be already filled
+            obj.renderOrder = 100
         });
-});
+    },1);
 
     return <><group ref={groupRef}>{children}</group></>;
 };
